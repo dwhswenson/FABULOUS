14e701de13b20cd0f33f250c32ce9b834bd5f15e
David W.H. Swenson <dwhs@hyperblazer.net>
2021-03-27 11:46:55 +0100

    fix missing colon

--- ops_utilities.py
+++ ops_utilities.py
@@ -36,7 +36,7 @@ def _get_keep_atoms(topology, keep):
         return [topology.atom(i) for i in keep]
 
 
-def extract_MD(trajectory, ref, keep_atoms)
+def extract_MD(trajectory, ref, keep_atoms):
     """Create an MD dataframe for FABULOUS from an OPS trajectory.
 
     Returns
